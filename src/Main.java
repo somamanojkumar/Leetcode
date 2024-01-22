dc0855c0acb150f33c9d3920e1c5830c71c48820
somamanojkumar <55225202+somamanojkumar@users.noreply.github.com>
2024-01-22 13:59:23 -0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter size: ");
+        int size = scanner.nextInt();
+        int[] arr = new int[size];
+        System.out.print("Enter elements: ");
+        for(int i = 0; i < size; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        Solution solution = new Solution();
+        int[] result = solution.productExceptSelf(arr);
+        System.out.print("Result: ");
+        for(int num : result) {
+            System.out.print(num + " ");
+        }
+    }
+}
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] left = new int[nums.length];
+        int[] right = new int[nums.length];
+        left[0] = 1;
+        for(int i = 1; i < nums.length; i++) {
+            left[i] = left[i - 1] * nums[i - 1];
+        }
+        right[nums.length - 1] = 1;
+        for(int i = nums.length - 2; i >= 0; i--) {
+            right[i] = right[i + 1] * nums[i + 1];
+        }
+        int[] result = new int[nums.length];
+        for(int i = 0; i < nums.length; i++) {
+            result[i] = left[i] * right[i];
+        }
+        return result;
+    }
+}
